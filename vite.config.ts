43533746a47a83dd0ea5a10b933187bb6339a3cc
1rate <a.070604kam@gmail.com>
2026-05-11 16:39:00 +0300

    fix: added rolldown

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -33,6 +33,7 @@ export default defineConfig({
           }
         },
       },
+      external: [],
     },
   },
 });
