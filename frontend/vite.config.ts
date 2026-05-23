62aa972ed425bed216c9f3e89fdde79aef41b292
1rate <a.070604kam@gmail.com>
2026-05-23 16:01:46 +0300

    feat: develop deploy to actions

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/graduation_project/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
